7a3b9a29a03ccaaae3dee514cbb0b85489798b48
vr62 <vr62@njit.edu>
2023-05-02 20:02:00 -0400

    chore: added ga analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
 import '../styles/global.css'
+import Script from 'next/script'
+
+const GA_TRACKING_ID = 'G-WG41M5B63B'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+    <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+          `,
+      }}
+    />
+    <Component {...pageProps} />
+  </>
 }

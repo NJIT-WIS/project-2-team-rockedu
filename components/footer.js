2e095acd7e97f6e392fad7473d23d8ece3c74db2
vr62 <vr62@njit.edu>
2023-05-07 18:52:14 -0400

    chore: footer and header added

---
+++ components/footer.js
@@ -0,0 +1,40 @@
+import React, { FC, ReactElement } from "react";
+import { Box, Container, Grid, Typography } from "@mui/material";
+
+const AuthorNames = ["Varshit", "Nandini", "Ramya"]
+
+export const Footer = () => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "auto",
+        backgroundColor: "#f5f5f5",
+        paddingTop: "1rem",
+        paddingBottom: "1rem",
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container direction="column" alignItems="center">
+          <Grid item xs={12}>
+            <Typography color="black" variant="h5">
+              MyWebClass.org
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography color="textSecondary" variant="subtitle1">
+              {`${new Date().getFullYear()} | React | Material UI | React Router`}
+            </Typography>
+            <Typography color="textSecondary" variant="subtitle1">
+              {`Made with ❤️ by `}{AuthorNames.map((name, index) => {
+                return <span key={index}>{name}{index < AuthorNames.length - 1 ? ", " : ""}</span>
+              })}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,61 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import { Box, Button, Typography } from '@mui/material'
+import ResponsiveAppBar from '../components/header'
 
 export default function Home({ allPostsData }) {
   return (
-    <Layout home>
+    <Layout home noNameImage>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      {/* section with heading */}
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Revolutionize Education
+        </Typography>
+        <Typography variant="h5" component="h2" gutterBottom>
+          Empower Your Classroom with Easy Daily Management!
+        </Typography>
+        {/* Box with two columns */}
+        {[{
+          title: "The Future",
+          description: "Explore how AI-driven agile and lean methodologies are reshaping education by making learning more personalized, adaptive, and efficient. We argue that school administrators need to adopt these approaches to stay competitive and meet the changing needs of students. Our Blog posts highlight the positive impact of AI-enhanced agile and lean approaches on learning outcomes, engagement, and resource utilization, revolutionizing the traditional education system.",
+          button: "Go to Blogs",
+          link: "/posts",
+          image: "/images/ai.jpg",
+          reversed: false
+        },
+        {
+          title: "Storytellers",
+          description: "At mywebclass.org, we are dedicated to educating teachers about the use of Artificial Intelligence (AI) in the classroom. Our mission is to help schools and educators provide better opportunities for their students by leveraging the power of AI. We offer a range of resources that are designed to help teachers learn about the fundamentals of AI, its applications in the classroom, and how it can be integrated into their teaching methods. Our team of experienced instructors are committed to providing high-quality education and support to help teachers feel confident and equipped to bring AI into their classrooms. Join us in this exciting journey towards a more innovative and inclusive future for education.",
+          button: "Go to About Us",
+          link: "/about",
+          image: "/images/ai.jpg",
+          reversed: true
+        }].map((item, index) => (
+          <Box sx={{ display: 'flex', flexDirection: item.reversed ? "row-reverse" : "row", justifyContent: 'space-between', my: 2 }}>
+            <Box sx={{ width: '50%', p: 1 }}>
+              <Typography variant="h4" component="h3" gutterBottom>
+                {item.title}
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                {item.description}
+              </Typography>
+              <Button variant="contained" color="primary" href={item.link}>
+                {item.button}
+              </Button>
+            </Box>
+            <Box sx={{ width: '50%', p: 1 }}>
+              {/* Box with blue bg */}
+              <Box sx={{ bgcolor: 'primary.main', color: 'primary.contrastText', p: 2, borderRadius: 1, height: "100%" }}>
+              </Box>
+            </Box>
+          </Box>))}
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+
+      {/* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
@@ -31,7 +71,7 @@ export default function Home({ allPostsData }) {
             </li>
           ))}
         </ul>
-      </section>
+      </section> */}
     </Layout>
   )
 }

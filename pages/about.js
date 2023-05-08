c0e2ddcc02699b9ede64e527765cf01ef19fa4e5
Nandini P <123400411+NandiniPalwai19@users.noreply.github.com>
2023-05-08 12:37:10 -0500

    about page fix

--- pages/about.js
+++ pages/about.js
@@ -32,6 +32,63 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
       </section>
+      <section>
+      <div style="display:flex;">
+  <div style="flex:1;">
+    <img src="/NJIT-WIS/project-2-team-rockedu/public/images/ldapimage.png" alt="Alt text" style="border-radius:60%; width:130px; height:130px; object-fit:cover;margin-bottom:30px;"/>
+  </div>
+
+  <div style="flex:1;">
+    <ul>
+      <li><b>Name:</b> Keith Williams</li>
+      <li><b>Designation:</b> Professor at NJIT</li>
+      <li>15+ years of Industry and Teaching Experience</li>
+    </ul>
+  </div>
+</div>
+
+<div style="display:flex;">
+  <div style="flex:1;">
+    <img src="/NJIT-WIS/project-2-team-rockedu/public/images/varshith.jpeg" alt="Alt text" style="border-radius:60%; width:130px; height:130px; object-fit:cover;margin-bottom:30px;"/>
+  </div>
+  <div style="flex:1;">
+    <ul>
+      <li><b>Name:</b> Varshith Reddy Regatte</li>
+      <li><b>Designation:</b> Student at NJIT</li>
+      <li>Fresh Graduate from NJIT expertise in Data Science field.</li>
+    </ul>
+  </div>
+</div>
+
+
+<div style="display:flex;">
+  <div style="flex:1;">
+    <img src="/NJIT-WIS/project-2-team-rockedu/public/images/nandini.jpeg" alt="Alt text" style="border-radius:60%; width:130px; height:130px; object-fit:cover;margin-bottom:30px;"/>
+  </div>
+  <div style="flex:1;">
+    <ul>
+      <li><b>Name:</b> Nandini Palwai</li>
+      <li><b>Designation:</b> Student at NJIT</li>
+      <li>Fresh Graduate from NJIT expertise in Data Science field.</li>
+    </ul>
+  </div>
+</div>
+
+
+<div style="display:flex;">
+  <div style="flex:1;">
+    <img src="/NJIT-WIS/project-2-team-rockedu/public/images/ramya.jpeg" alt="Alt text" style="border-radius:60%; width:130px; height:130px; object-fit:cover;margin-bottom:30px;"/>
+  </div>
+  <div style="flex:1;">
+    <ul>
+      <li><b>Name:</b> Ramya Kammari</li>
+      <li><b>Designation:</b> Student at NJIT</li>
+      <li>Fresh Graduate from NJIT expertise in Information Systems.</li>
+    </ul>
+  </div>
+</div>
+
+      </section>
     </Layout>
   )
 }
